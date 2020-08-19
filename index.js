28033795bad40dbf86c989eb5558a45d2f05f41e
vinivyes <32873823+vinivyes@users.noreply.github.com>
2020-08-19 10:26:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
 
 
-let benchmark_requests = 1;
+let benchmark_requests = 1;  //Cantidad de requests simultaneos a enviar.
+let clients_in_db = 10000; //Se genera 
+let clientes = true; //Consulta por clientes ? cambie a 'false' para consultar por ciudad
+
 
 const request = require('request');
 
+//#region - Testeo de consultas de clientes por ciudad
+
+//Consulta a la base de datos
 const BenchmarkDB = (test) => {
     let total_time = 0;
     let requests = 0;
     let s = 0;
 
     for(let r = 0; r < test; r++)
-    request(`http://207.246.69.202:2550/bd_ciudad/San%20Jose`, (err, res, body) => {
+    request(clientes ? `http://207.246.69.202:2550/bd_cliente/${Math.floor(Math.random()*clients_in_db)}` : `http://207.246.69.202:2550/bd_ciudad/San%20Jose`, (err, res, body) => {
         if(err == null){
             total_time += Number.parseFloat(body);
             requests += 1;
         }
         s++;
         if(s == test){
-            console.log(`Average of ${total_time / requests }ms on BD query for ${benchmark_requests} simultaneous queries;`)
-            BenchmarkCache(benchmark_requests)
+            console.log(`En promedio la consulta duro ${total_time / requests }ms en la Base de datos para ${benchmark_requests} consultas simultaneas`)
+            BenchmarkCache(benchmark_requests) //Consulta a el cache inicializada despues de que termine la consulta a la base de datos, reduz ruido en los resultados finales.
         }
     });
 }
+
+//Consulta a el cache
 const BenchmarkCache = (test) => {
     let total_time = 0;
     let requests = 0;
     let s = 0;
 
     for(let r = 0; r < test; r++)
-    request(`http://207.246.69.202:2550/cache_ciudad/San%20Jose`, (err, res, body) => {  
+    request(clientes ? `http://207.246.69.202:2550/cache_cliente/${Math.floor(Math.random()*clients_in_db)}` : `http://207.246.69.202:2550/cache_ciudad/San%20Jose`, (err, res, body) => {  
         if(err == null){
             total_time += Number.parseFloat(body);
             requests += 1;
         }
         s++;
         if(s == test){ 
-            console.log(`Average of ${total_time / requests}ms on cache query for ${benchmark_requests} simultaneous queries;`)
+            console.log(`En promedio la consulta duro ${total_time / requests }ms en la Base de datos para ${benchmark_requests} consultas simultaneas`)
         }
     });
 
 }
-BenchmarkDB(benchmark_requests)
+
+//#endregion - Testeo de consultas de clientes por ciudad
+
+
+BenchmarkDB(benchmark_requests)  //Iniciar benchmark con la base de datos
